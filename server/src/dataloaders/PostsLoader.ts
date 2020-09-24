18f61a0fadf22922c98abfb2c2b11b3d4f8487c6
Purbarag Choudhury <pchoudhury@zaloni.com>
2020-09-25 02:53:54 +0530

    Initial commit

--- server/src/dataloaders/PostsLoader.ts
+++ server/src/dataloaders/PostsLoader.ts
@@ -1,6 +1,17 @@
-// import DataLoader from "dataloader";
-// import { Post } from "./../entities/Post";
+import DataLoader from "dataloader";
+import { Post } from "../entities/Post";
 
-// export const PostsLoader = () => {
-//   new DataLoader<number, Post>
-// }
+export const PostsLoader = new DataLoader<number, Post>(async (userIds) => {
+  console.log("Resolver with dataloader called");
+  const posts = await Post.find({where: {'authorId': {$in: userIds}}});
+  const groupedPosts = posts.reduce((arr, post) => {
+    arr[post.authorId] = arr[post.authorId] || [];
+    arr[post.authorId].push(post);
+    return arr;
+  }, Object.create(null));
+  const sortedPosts = userIds.map((userId) => typeof groupedPosts[userId] !== 'undefined' ? groupedPosts[userId] : []);
+  console.log("GroupedPosts:", sortedPosts);
+  console.log("Typeof GroupedPosts:", typeof sortedPosts);
+  return sortedPosts;
+});
+
