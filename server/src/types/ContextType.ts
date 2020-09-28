cc1a9cc70707e9ad3ebdc040d0e70c3e0ab11709
Purbarag Choudhury <pchoudhury@zaloni.com>
2020-09-29 03:59:19 +0530

    Refactored context type

--- server/src/types/ContextType.ts
+++ server/src/types/ContextType.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
+import { IUser } from "./TokenUser";
+
+export interface AuthUserInfoRequest extends Request {
+  user: IUser
+}
 
 export type ContextType = {
-  req: Request;
+  req: AuthUserInfoRequest;
   res: Response;
 };
