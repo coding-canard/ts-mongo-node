2bc82249fd85dd68bbf45ac8cd65203698a5ed7b
Purbarag Choudhury <pchoudhury@zaloni.com>
2020-09-29 03:56:57 +0530

    Added more constants

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,2 +1,11 @@
 export const __PROD__ = process.env.NODE_ENV === "production";
-export const SESSION_SECRET = "notagoodsecret";
+export const ACCESS_COOKIE_NAME = "x-access-cookie";
+export const REFRESH_COOKIE_NAME = "x-refresh-cookie";
+export const ACCESS_SECRET = "notagoodaccesssecret";
+export const REFRESH_SECRET = "notagoodrefreshsecret";
+export const ACCESS_TOKEN_EXPIRES = "15m";
+export const REFRESH_TOKEN_EXPIRES = "7d";
+// export const ACCESS_TOKEN_EXPIRES = new Date(Date.now() + 1000 * 60 * 15) // 15 minutes
+// export const REFRESH_TOKEN_EXPIRES = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7) // 7 days
+export const ACCESS_COOKIE_EXPIRES = 1000 * 60 * 15 // 15 minutes
+export const REFRESH_COOKIE_EXPIRES = 1000 * 60 * 60 * 24 * 7 // 7 days
