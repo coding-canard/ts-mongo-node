87c98ed39d735289a66b1bebcab1eb8729e225fd
Purbarag Choudhury <pchoudhury@zaloni.com>
2020-10-03 01:33:42 +0530

    New middleware.

---
+++ server/src/middlewares/IsAuthor.ts
@@ -0,0 +1,13 @@
+import { MiddlewareFn, NextFn, ResolverData } from "type-graphql";
+import { Post } from "../entities/Post";
+import { ContextType } from "../types/ContextType";
+
+export const IsAuthor = (message?: string): MiddlewareFn<ContextType> => async ({ context, args }: ResolverData<ContextType>, next: NextFn) => {
+
+  const count = await Post.count({ id: args.id, authorId: context.req.user.id});
+
+  if(count === 0){
+    throw new Error(message ? message : "Unauthorized - You cannot perform this action.");
+  }
+  return next();
+  };
