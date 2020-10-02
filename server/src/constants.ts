e28a766539ffebbbd53fcf39d3723a8046eca572
Purbarag Choudhury <pchoudhury@zaloni.com>
2020-10-02 21:02:58 +0530

    Moved secret to env file

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,8 +1,6 @@
 export const __PROD__ = process.env.NODE_ENV === "production";
 export const ACCESS_COOKIE_NAME = "x-access-cookie";
 export const REFRESH_COOKIE_NAME = "x-refresh-cookie";
-export const ACCESS_SECRET = "notagoodaccesssecret";
-export const REFRESH_SECRET = "notagoodrefreshsecret";
 export const ACCESS_TOKEN_EXPIRES = "15m";
 export const REFRESH_TOKEN_EXPIRES = "7d";
 // export const ACCESS_TOKEN_EXPIRES = new Date(Date.now() + 1000 * 60 * 15) // 15 minutes
