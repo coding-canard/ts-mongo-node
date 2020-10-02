f96b6b527843872fd3767592cc19215ed57c4b42
Purbarag Choudhury <pchoudhury@zaloni.com>
2020-10-03 01:31:46 +0530

    Minor tweak

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,8 +2,6 @@ export const __PROD__ = process.env.NODE_ENV === "production";
 export const ACCESS_COOKIE_NAME = "x-access-cookie";
 export const REFRESH_COOKIE_NAME = "x-refresh-cookie";
 export const ACCESS_TOKEN_EXPIRES = "15m";
-export const REFRESH_TOKEN_EXPIRES = "7d";
-// export const ACCESS_TOKEN_EXPIRES = new Date(Date.now() + 1000 * 60 * 15) // 15 minutes
-// export const REFRESH_TOKEN_EXPIRES = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7) // 7 days
+export const REFRESH_TOKEN_EXPIRES = "1d";
 export const ACCESS_COOKIE_EXPIRES = 1000 * 60 * 15 // 15 minutes
-export const REFRESH_COOKIE_EXPIRES = 1000 * 60 * 60 * 24 * 7 // 7 days
+export const REFRESH_COOKIE_EXPIRES = 1000 * 60 * 60 * 24 * 1 // 1 days
